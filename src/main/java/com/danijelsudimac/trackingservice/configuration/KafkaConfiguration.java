0e6b4d720d2b1c54d26d771a9dc01cc3b839e95e
Danijel Sudimac <dacha.sudimac@gmail.com>
2026-06-19 10:29:21 +0200

    fix: Missing kafka template

--- src/main/java/com/danijelsudimac/trackingservice/configuration/KafkaConfiguration.java
+++ src/main/java/com/danijelsudimac/trackingservice/configuration/KafkaConfiguration.java
@@ -1,6 +1,7 @@
 package com.danijelsudimac.trackingservice.configuration;
 
 import com.danijelsudimac.trackingservice.service.TrackingMetrics;
+import com.google.protobuf.GeneratedMessage;
 import jakarta.validation.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -57,37 +58,6 @@ public class KafkaConfiguration {
     }
 
     @Bean
-    public DefaultErrorHandler errorHandler(KafkaTemplate<String, byte[]> poisonTemplate,
-                                            KafkaTemplate<Object, Object> template) {
-
-        var recoverer =
-                new DeadLetterPublishingRecoverer(Map.of(Object.class, template, byte[].class, poisonTemplate),
-                        (record, ex) -> {
-                            Throwable rootException = Optional.ofNullable(ex.getCause()).orElse(ex);
-                            var topic = resolveTopic(rootException);
-                            log.warn(ERROR_LOG_MESSAGE, record.key(), rootException.getClass(), rootException.getMessage(), topic);
-                            trackingMetrics.incrementMessageInDlt();
-                            return new TopicPartition(topic, record.partition());
-                        });
-        var errorHandler = new DefaultErrorHandler(recoverer, new FixedBackOff(1000L, 3));
-        errorHandler.setCommitRecovered(true);
-        errorHandler.addNotRetryableExceptions(
-                IllegalArgumentException.class,
-                DeserializationException.class,
-                ValidationException.class
-        );
-
-        return errorHandler;
-    }
-
-    private String resolveTopic(Throwable ex) {
-        return INVALID_MESSAGE_EXCEPTIONS.stream()
-                .anyMatch(clazz -> clazz.isInstance(ex))
-                ? topicInvalidMessages
-                : topicDtl;
-    }
-
-    @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
             ConsumerFactory<String, String> consumerFactory, DefaultErrorHandler errorHandler) {
         ConcurrentKafkaListenerContainerFactory<String, String> factory =
@@ -112,5 +82,41 @@ public class KafkaConfiguration {
     @Bean
     public KafkaTemplate<String, byte[]> poisonTemplate(ProducerFactory<String, byte[]> poisonProducerFactory) {
         return new KafkaTemplate<>(poisonProducerFactory);
+    }
+
+    @Bean
+    public KafkaTemplate<Object, Object> kafkaTemplate(ProducerFactory<Object, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    public DefaultErrorHandler errorHandler(KafkaTemplate<String, byte[]> poisonTemplate,
+                                            KafkaTemplate<Object, Object> kafkaTemplate) {
+
+        var recoverer =
+                new DeadLetterPublishingRecoverer(Map.of(GeneratedMessage.class, kafkaTemplate, byte[].class, poisonTemplate),
+                        (record, ex) -> {
+                            Throwable rootException = Optional.ofNullable(ex.getCause()).orElse(ex);
+                            var topic = resolveTopic(rootException);
+                            log.warn(ERROR_LOG_MESSAGE, record.key(), rootException.getClass(), rootException.getMessage(), topic);
+                            trackingMetrics.incrementMessageInDlt();
+                            return new TopicPartition(topic, record.partition());
+                        });
+        var errorHandler = new DefaultErrorHandler(recoverer, new FixedBackOff(1000L, 3));
+        errorHandler.setCommitRecovered(true);
+        errorHandler.addNotRetryableExceptions(
+                IllegalArgumentException.class,
+                DeserializationException.class,
+                ValidationException.class
+        );
+
+        return errorHandler;
+    }
+
+    private String resolveTopic(Throwable ex) {
+        return INVALID_MESSAGE_EXCEPTIONS.stream()
+                .anyMatch(clazz -> clazz.isInstance(ex))
+                ? topicInvalidMessages
+                : topicDtl;
     }
 }
