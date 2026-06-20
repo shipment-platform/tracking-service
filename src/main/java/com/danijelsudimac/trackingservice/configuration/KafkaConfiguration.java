d73f46d861fd2b0ab9a2fa34fd6e5c58da48b470
Danijel Sudimac <dacha.sudimac@gmail.com>
2026-06-21 01:35:43 +0200

    feat: Add k82 config

--- src/main/java/com/danijelsudimac/trackingservice/configuration/KafkaConfiguration.java
+++ src/main/java/com/danijelsudimac/trackingservice/configuration/KafkaConfiguration.java
@@ -13,12 +13,9 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.DeserializationException;
@@ -55,20 +52,6 @@ public class KafkaConfiguration {
         this.topicDtl = topicDtl;
         this.topicInvalidMessages = topicInvalidMessages;
         this.trackingMetrics = trackingMetrics;
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(
-            ConsumerFactory<String, String> consumerFactory, DefaultErrorHandler errorHandler) {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory);
-        factory.setCommonErrorHandler(errorHandler);
-        factory.setConcurrency(3);
-        factory.setBatchListener(false);
-        factory.getContainerProperties().setMissingTopicsFatal(false);
-        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.RECORD);
-        return factory;
     }
 
     @Bean
